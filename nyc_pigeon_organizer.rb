0472c6586bf70eb8d66402076689b1bbf4074be4
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-26 21:07:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+new_hash={}
+data.each do |key, value|
+  value.each do |key1, value1|
+    value1.each do |name|
+      if new_hash[name]==nil 
+     new_hash[name]={}
+     end 
+  if new_hash[name][key]==nil 
+    new_hash[name][key]=[]
+  end
+     new_hash[name][key].push(key1.to_s)
+   end 
+ end 
+ end 
+ new_hash 
+ end 
+ 
+ 
