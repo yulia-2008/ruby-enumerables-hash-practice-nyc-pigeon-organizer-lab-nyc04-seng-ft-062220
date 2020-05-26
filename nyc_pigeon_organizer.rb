de9df949c9cac3aa911537f97f0e667a711ff272
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-26 20:18:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ new_hash={}
 data.each do |key, value|
   value.each do |key1, value1|
     value1.each do |name|
-      if new_hash[name]==nil 
+       
      new_hash[name]={}
-    end 
-    if new_hash[name][key]==nil 
+    
+    
       new_hash[name][key]=[]
-    end
+    
      new_hash[name][key].push(key1.to_s)
    end 
  end 
